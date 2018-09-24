1a88c9be8448885898321df773f955f2142ccb42
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2018-09-24 23:04:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ let b=s[3]+s[4];
 let c=s[7]+s[8];
 
  
-x1=(-b+Math.sqrt(b*b-4*a*c))/(2*a);
-x2=(-b-Math.sqrt(b*b-4*a*c))/(2*a);
+x1=Math.round((-b+Math.sqrt(b*b-4*a*c))/(2*a));
+x2=Math.round((-b-Math.sqrt(b*b-4*a*c))/(2*a));
 if (x1 < x2) return [x1, x2]; else return [x2, x1];
 
 }
