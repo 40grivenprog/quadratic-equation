01f635f9f4efa262eed5330a0ca9884addc8ade1
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2018-09-24 22:58:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var s = equation.split(' ');
+
+let x1=0 ;
+let x2=0 ;
+let a=s[0]; 
+let b=s[3]+s[4];
+let c=s[7]+s[8];
+
+ 
+x1=(-b+Math.sqrt(b*b-4*a*c))/(2*a);
+x2=(-b-Math.sqrt(b*b-4*a*c))/(2*a);
+return [x2, x1];
+
 }
+// solutions1 = solveEquation('-20 * x^2 - 108797540 * x - 130011773690520');
+//alert(solutions1); 
